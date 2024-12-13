bc59046127e67979166090b873be2e294d29d11c
aqjo <hamadicharef.elisa@gmail.com>
2024-12-13 14:21:53 +0100

    Refonte de la galerie avec ajout de filtres et amélioration du style CSS

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,34 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+function init() {
+   
+        lightGallery(document.getElementById('lightgallery'), {
+            licenseKey: '0000-0000-000-0000',
+        });
+    
+        const filterButtons = document.querySelectorAll('.filter-button');
+        
+        const handleFilterClick = (button) => {
+            
+                const filter = button.getAttribute('data-filter');
+
+                filterButtons.forEach(btn => btn.classList.remove('active'));
+                button.classList.add('active');
+
+                const galleryItems = document.querySelectorAll('.gallery-item');
+    
+                galleryItems.forEach(item => {
+                    const tags = item.getAttribute('data-tags') ? item.getAttribute('data-tags').split(' ') : [];
+    
+                    if (filter === 'all' || tags.includes(filter)) {
+                        item.classList.remove('hidden');
+                    } else {
+                        item.classList.add('hidden');
+                    }
+                });
+        };
+    
+        filterButtons.forEach(button => {
+            button.addEventListener('click', () => handleFilterClick(button));
+        });
+}
+document.addEventListener("DOMContentLoaded", init());
+
